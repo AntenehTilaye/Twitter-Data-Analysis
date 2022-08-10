ee90bb476eea9fc3cfbbaaf5f6966144fc790957
anteneh tilaye <antenehtilaye19@gmail.com>
2022-08-10 11:37:12 +0300

    json file format

--- create_json.py
+++ create_json.py
@@ -4,7 +4,8 @@ import json
 with open('tweet_sample.json', 'w') as f:
     i = 0
     for tweets in open('data/africa_twitter_data.json'):
-        json.dump(json.loads(tweets), f, indent=2)
+        print(tweets)
+        json.dump(json.loads(tweets), f, indent=0)
         i = i + 1
         
         if(i == 10):
